046091f5687162adb1b98f6392e9b5bf828be717
Akwesi-bonah <arhinbonnah@gmail.com>
2023-08-31 18:11:51 +0000

    task 14 error fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+size_t binary_tree_height(const binary_tree_t *tree);
 /**
  * binary_tree_balance - Measures the balance factor
  * @tree: A pointer to the root node of the tree to measure the balance factor.
@@ -25,8 +26,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
